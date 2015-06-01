ae21aaec6e4e3e792450b4b96856b9e1b1deda63
Tracy Poff <tracy.poff@gmail.com>
2015-05-31 21:39:34 -0400

    Remove unused imports.

--- archery.py
+++ archery.py
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with CGRR.  If not, see <http://www.gnu.org/licenses/>.
 """Parses Archery files."""
-import logging
-import struct
 import os
 
 import cgrr
