3b16a4c0268d398be43273838988f60d11f61e3a
Tracy Poff <tracy.poff@gmail.com>
2021-01-20 13:55:40 -0500

    Increase test coverage.

--- test_archery.py
+++ test_archery.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with CGRR.  If not, see <http://www.gnu.org/licenses/>.
 import logging
+import pytest
 
 class Test_archery_a:
 
@@ -30,46 +31,7 @@ class Test_archery_a:
                 b'Game  27tiny       26rascal      1').ljust(256, b'\0')
         self.scorefile = io.BytesIO(mock)
 
-    def teardown(self):
-        self.plugin = None
-        self.scorefile = None
-
-    def test_extract_scores_from_path(self):
-        """Test whether extract_scores opens the right file, given a path."""
-        import os
-        try:
-            from unittest.mock import mock_open, patch
-        except ImportError:
-            from nose import SkipTest
-            raise SkipTest
-        with patch('builtins.open', mock_open(), create=True) as m:
-            try:
-                self.archery.extract_scores("foo")
-            except TypeError:
-                # mock_open makes an object with a faulty read method (doing
-                # read(n) returns the whole file rather than just the next n
-                # bytes), so rather than try to fix that, just don't bother with
-                # the actual data, and catch the error that causes.
-                pass
-            m.assert_called_once_with(os.path.join("foo", "ARCHERY.SCR"), 'rb')
-
-    def test_extract_scores_from_scorepath(self):
-        """Test whether extract_scores opens the scorepath given."""
-        try:
-            from unittest.mock import mock_open, patch
-        except ImportError:
-            from nose import SkipTest
-            raise SkipTest
-        with patch('builtins.open', mock_open(), create=True) as m:
-            try:
-                self.archery.extract_scores(scorepath="foo")
-            except TypeError:
-                pass
-            m.assert_called_once_with("foo", 'rb')
-
-    def test_read_scores(self):
-        """Test whether read_scores correctly interprets known scores."""
-        correct = [
+        self.correct = [
             {'score': 84, 'name': 'Computer'},
             {'score': 47, 'name': 'MobyGamer'},
             {'score': 39, 'name': 'Moby Game'},
@@ -79,9 +41,40 @@ class Test_archery_a:
             {'score': 1, 'name': 'rascal'}
         ]
 
+
+    def teardown(self):
+        self.plugin = None
+        self.scorefile = None
+
+    def test_extract_scores_from_path(self):
+        """Test whether extract_scores opens the right file, given a path."""
+        import os
+        from unittest.mock import mock_open, patch
+        with patch('builtins.open',
+                   mock_open(read_data=self.scorefile.getvalue()),
+                   create=True) as m:
+            scores = self.archery.extract_scores("foo")
+            m.assert_called_once_with(os.path.join("foo", "ARCHERY.SCR"), 'rb')
+
+        assert scores == self.correct
+
+    def test_extract_scores_from_scorepath(self):
+        """Test whether extract_scores opens the scorepath given."""
+        from unittest.mock import mock_open, patch
+        with patch('builtins.open',
+                   mock_open(read_data=self.scorefile.getvalue()),
+                   create=True) as m:
+            scores = self.archery.extract_scores(scorepath="foo")
+            m.assert_called_once_with("foo", 'rb')
+
+        assert scores == self.correct
+
+    def test_read_scores(self):
+        """Test whether read_scores correctly interprets known scores."""
+
         actual = self.archery.read_scores(self.scorefile)
 
-        assert actual == correct
+        assert actual == self.correct
 
     def test_write_scores(self):
         """Roundtrip a score file with read_scores and write_scores."""
@@ -89,3 +82,15 @@ class Test_archery_a:
         actual = self.archery.write_scores(scores)
 
         assert actual == self.scorefile.getvalue()
+
+    def test_extract_scores_without_path(self):
+        with pytest.raises(ValueError) as e:
+            self.archery.extract_scores()
+
+        assert str(e.value) == "Either path or scorefile must be specified."
+
+    def test_extract_scores_with_path_and_scorepath(self):
+        with pytest.raises(ValueError) as e:
+            self.archery.extract_scores(path='foo', scorepath='bar')
+
+        assert str(e.value) == "Only one of path or scorefile may be specified."
